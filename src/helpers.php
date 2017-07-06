e8e279cb97a7f93b79a862baa7a5960d7f8c8614
Devon Weller <devon@tokenly.co>
2017-07-05 20:11:43 -0500

    a little cleanup

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,11 @@
 <?php
 
-if (! function_exists('fluent_measure')) {
+if (!function_exists('fluent_measure')) {
+
     /**
      * logs a measurement to fluentd
+     * A Laravel helper
+     * 
      * @param  string $event a measurement name like widgets.created
      * @param  array  $data  an array of numeric data.  All entries in this array must be an integer or float.
      * @param  array  $tags  an array of tags.  All tags are lookup fields such as userId or widgetType.
@@ -10,4 +13,5 @@ if (! function_exists('fluent_measure')) {
     function fluent_measure($event, $data=[], $tags=null) {
         app('fluent.measurements')->log($event, $data, $tags);
     }
+
 }
